c62fd6dd6f12506c6fa879b0ae9f5eb00db79988
ecVictor7 <vb@eco-charge.eu>
2024-12-20 10:11:57 +0100

    version 1.0

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Starting my System monitor...")
+	go func() {
+		for {
+			systemSection, err := hardware.GetSystemSection()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			diskSection, err := hardware.GetDiskSection()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println(systemSection)
+			fmt.Println(diskSection)
+			fmt.Println(cpuSection)
+
+			time.Sleep(3 * time.Second)
+		}
+	}()
+	//sleep the main threead
+	time.Sleep(5 * time.Minute)
+}
