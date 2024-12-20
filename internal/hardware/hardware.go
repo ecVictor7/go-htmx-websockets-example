193d3948d263573c42415d0bf350ebb5997195e4
ecVictor7 <vb@eco-charge.eu>
2024-12-20 10:21:54 +0100

    version 1.2

--- internal/hardware/hardware.go
+++ internal/hardware/hardware.go
@@ -5,6 +5,7 @@ import (
 	"runtime"
 
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
 )
@@ -32,4 +33,15 @@ func GetCpuSection() (string, error) {
 	if err != nil {
 		return "", err
 	}
+	output := fmt.Sprintf("CPU: %s\nCores: %d", cpuStat[0].ModelName, len(cpuStat))
+	return output, nil
+}
+
+func GetDiskSection() (string, error) {
+	diskStat, err := disk.Usage("/")
+	if err != nil {
+		return "", err
+	}
+	output := fmt.Sprintf("Total DIsk Space: %d\nFree Disk Space: %d", diskStat.Total, diskStat.Free)
+	return output, nil
 }
